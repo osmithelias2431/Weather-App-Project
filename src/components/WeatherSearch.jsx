c7db4244fc5ed2ea06e7d77ba1965ccbbf4b658e
Oneilia Smith-Elias <osmithelias@outlook.com>
2025-08-19 00:14:47 -0400

    Removed react & vite html content, added components folder & weather search component

---
+++ src/components/WeatherSearch.jsx
@@ -0,0 +1,20 @@
+function WeatherSearch({weather}) {
+
+    function clickSearch() {
+        alert("Clicked!");
+    }
+
+    return <div className="weather-search">
+        <h2>Weather Now</h2>
+        <div className="weather-icon">
+            <img src={weather.icon} alt="Weather Icon" />
+            <div className="search">
+                <input type="text" placeholder="Search for a city..." />
+                <button className="search-button" onClick={clickSearch}>
+                </button>
+            </div>
+        </div>
+    </div>
+}
+
+export default WeatherSearch;
