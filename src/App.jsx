3e464d90bdb8f2965954616ae3af30908a036571
Oneilia Smith-Elias <osmithelias@outlook.com>
2025-08-25 15:15:53 -0400

    Added styling to search bar and grid to extra weather details

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,9 @@ function App() {
     <>
     <div className="background-card">
       <div className="background-image"></div>
+      <h2 className="title">Weather App</h2>
       <form onSubmit={handleSearch} className="form">
-        <input type="text" placeholder="Search for a city..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+        <input type="text" placeholder="Search for a city..." className="search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
         <button type="submit" className="search-button">Search</button>
       </form>
       <WeatherDisplay display={{}} />
